d0f82e0c9ad61ca79e1baffaef48ac000a98868d
anzhiyong <2056048952@qq.com>
2023-10-21 20:00:47 +0800

    更改

--- Project1/Project1/作业四.c
+++ Project1/Project1/作业四.c
@@ -53,16 +53,19 @@
 //#include <stdio.h>
 //void main() {
 //    int a = 2, b = 7, c = 5;
-//    switch (a > 0) {
-//    case 1: switch (b < 0) {
-//    case 1: printf("@"); break;
-//    case 2: printf("!"); break;
-//    }
-//    case 0: switch (c == 5) {
-//    case 1: printf("*"); break;
-//    case 2: printf("#"); break;
-//    default: printf("#"); break;
-//    }
+//    switch (a > 0)
+//    {
+//    case 1: switch (b < 0) 
+//            {
+//                case 1: printf("@"); break;
+//                case 2: printf("!"); break;
+//            }
+//    case 0: switch (c == 5) 
+//            {
+//                case 1: printf("*"); break;
+//                case 2: printf("#"); break;
+//                default: printf("#"); break;
+//            }
 //    default: printf("&");
 //    }
 //    printf("\n");
