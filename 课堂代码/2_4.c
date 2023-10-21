9ac132db976ded6a4c2a1c26302f08b9ade8b87c
anzhiyong <2056048952@qq.com>
2023-10-21 18:20:33 +0800

    课堂作业

---
+++ 课堂代码/2_4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main( )
+{ 
+int a,b;
+a=20;
+b=30;
+printf("(a+b)/(a-b)=%d\n", (a+b)/(a-b));
+printf("a+b/a-b=%d\n",a+b/a-b);
+return 0;
+}
+
---
+++ 课堂代码/3_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+int a=10,b=50,c=30;
+if (a>b)
+{
+a=b;
+b=c;
+c=a;
+}
+printf("a=%d,b=%d,c=%d\n",a,b,c);
+return 0;
+}
