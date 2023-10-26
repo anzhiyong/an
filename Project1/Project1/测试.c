2c48f3d95065fd46bc89fe2368a7baf0e6d0105a
anzhiyong <2056048952@qq.com>
2023-10-26 12:48:34 +0800

    额

--- Project1/Project1/测试.c
+++ Project1/Project1/测试.c
@@ -1,13 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h> 
+//int main() {
+//	char n;
+//	while ((n = getchar()) != EOF) {
+//		if (n >= 'A' && n <= 'Z' || n >= 'a' && n <= 'z') {
+//			printf("是字母!\n");
+//		}
+//		else {
+//			printf("不是字母！\n");
+//		}
+//		getchar();
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
 int main()
 {
-    int a = 7;
-    int b = 2;
-    float c = 2.0f;
-    printf("%d\n", 3);
-    printf("%f\n", a / c);
+	char cc;
+	scanf("%c", &cc);
+	if (cc >= 'A' && cc <= 'Z' || cc>='a' && cc <= 'z')
+		printf("是字母");
+	else
+		printf("不是字母");
 
-    return 0;
 }
---
+++ Project1/Project1/猜数字游戏.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	printf("ÇëÊäÈë±»²ÂÊı×Öa");
+	scanf("%d", &a);
+
+}
