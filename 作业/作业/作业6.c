9a02a89072b06365852044694848e91b4171ed1d
anzhiyong <2056048952@qq.com>
2023-11-08 19:52:29 +0800

    冒泡排序

--- 作业/作业/作业6.c
+++ 作业/作业/作业6.c
@@ -38,14 +38,28 @@
 #include<stdio.h>
 int main()
 {
-	int a[11];
-	int i = 0;
-	for (i = 1; i < 11; i++)
+	int a[10];
+	int i, j, temp;
+	for (i = 0; i < 10; i++)
 	{
-		scanf("%d",a[i]);
-
+		scanf("%d",&a[i]);
 	}
 	//Ã°ÅÝÅÅÐò
-
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i+1; j < 10; j++)
+		{
+			if (a[i] > a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%5d", a[i]);
+	}
 	
 }
