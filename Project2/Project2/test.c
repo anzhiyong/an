1ac2678d8193c93fccd89c7431257fb9a01d7d7e
anzhiyong <2056048952@qq.com>
2023-11-15 17:50:58 +0800

    test.c

--- Project2/Project2/test.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main()
-{
-	int i;
-	scanf("a=%d", &i);//https://edu.bitejiuyeke.com/login
-	printf("%d", i);
-}
--- test_11_13/test_11_13/进制.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-//int main()//交换两个变量（不创建临时变量）
-//{
-//	int a=5,b=9;
-//	a=a^b;
-//	b=a^b;
-//	a=a^b;
-//	printf("%d,%d",a,b);
-//}
