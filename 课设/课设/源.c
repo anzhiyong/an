53999041afef824280d58ed87ade28467fa4317a
大勇擎天柱 <2056048952@qq.com>
2023-12-01 01:13:28 +0000

    update 课设/课设/源.c.
    Signed-off-by: 大勇擎天柱 <2056048952@qq.com>

--- 课设/课设/源.c
+++ 课设/课设/源.c
@@ -1,13 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
+#include<conio.h> 
 
 #define N 40
 #define LEN sizeof(struct student) 
-#define FORMAT "%d %s %.2lf %.2lf %.2lf %.2lf\n"
-#define DATA stu[i].num,stu[i].name,stu[i].m,stu[i].e,stu[i].sum
+#define FORMAT "%d	%s	%.2lf	%.2lf	%.2lf	%.2lf\n"
+#define DATA stu[i].num,stu[i].name,stu[i].c,stu[i].m,stu[i].e,stu[i].sum 
 
+float Fc,Fm,Fe;
 struct student
 {
 	int num;
@@ -53,16 +55,18 @@ void main()/*主函数*/
 		   case 4: change();break;
 		   case 5:order();break;
 		   case 6:count();break;
-		default:break;  }
-		menu();/*执行完功能再次显示菜单界面*/
-		scanf("%d",&n);   }
+		   default:break;  
+		}
+		menu();
+		scanf("%d",&n);  
+	}
 }
 
-void in()/*自定义函数实现录入学生成绩信息*/
-{ 	int i,m=0;/*m是记录的条数*/
+void in()
+{ 	int i,m=0;
 	char ch[2];
-	FILE *fp;/*定义文件指针*/
-	if((fp=fopen("data","a+"))==NULL)/*打开指定文件*/
+	FILE *fp;
+	if((fp=fopen("data","a+"))==NULL)
 	{ 
 		printf("系统出错，请与开发人员联系！\n");
 		return;
@@ -70,7 +74,7 @@ void in()/*自定义函数实现录入学生成绩信息*/
 	while(!feof(fp)) 
 	{ 
 		if(fread(&stu[m] ,LEN,1,fp)==1)
-			m++;/*统计当前记录条数*/
+			m++;
 	}
 	fclose(fp);
 	if(m==0) 
@@ -78,7 +82,7 @@ void in()/*自定义函数实现录入学生成绩信息*/
 	else 
 	{
 		system("cls");
-		show();/*调用show函数，显示原有信息*/
+		show();
 	}
 	if((fp=fopen("data","a+"))==NULL)
 	{ 
@@ -87,10 +91,10 @@ void in()/*自定义函数实现录入学生成绩信息*/
 	}
 	printf("是否录入学生成绩，请按(y/n):");
 	scanf("%s",ch);
-	while(strcmp(ch,"Y")==0||strcmp(ch,"y")==0)/*判断是否要录入新信息*/
+	while(strcmp(ch,"Y")==0||strcmp(ch,"y")==0)
 	{
 		printf("请输入学生学号:");
-		scanf("%d",&stu[m].num);/*输入学生学号*/
+		scanf("%d",&stu[m].num);
 		for(i=0;i<m;i++)
 			if(stu[i].num==stu[m].num)
 			{
@@ -100,15 +104,15 @@ void in()/*自定义函数实现录入学生成绩信息*/
 				return;
 			}
 			printf("请输入学生姓名:");
-			scanf("%s",stu[m].name);/*输入学生姓名*/
+			scanf("%s",stu[m].name);
 			printf("C语言:");
-			scanf("%lf",&stu[m].c);/*输入C语言成绩*/
+			scanf("%lf",&stu[m].c);
 			printf("高数:");
-			scanf("%lf",&stu[m].m);/*输入高数成绩*/
+			scanf("%lf",&stu[m].m);
 			printf("英语:");
-			scanf("%lf",&stu[m].e);/*输入英语成绩*/
-			stu[m].sum=stu[m].c+stu[m].m+stu[m].e;/*计算出总成绩*/
-			if(fwrite(&stu[m],LEN,1,fp)!=1)/*将新录入的信息写入指定的磁盘文件*/
+			scanf("%lf",&stu[m].e);
+			stu[m].sum=stu[m].c+stu[m].m+stu[m].e;
+			if(fwrite(&stu[m],LEN,1,fp)!=1)
 			{
 				printf("不能保存，请与开发人员联系!");  
 			}
@@ -117,13 +121,13 @@ void in()/*自定义函数实现录入学生成绩信息*/
 				printf("%s成绩录入成功!\n",stu[m].name);
 				m++;
 			}
-			printf("是否继续录入?(y/n):");/*询问是否继续*/
+			printf("是否继续录入?(y/n):");
 			scanf("%s",ch);
 	}
 	fclose(fp);
 	
 }
-void show()//自定义函数实现显示学生成绩信息 
+void show()
 {
 	FILE* fp;
 	int i,m=0;
@@ -147,50 +151,200 @@ void search()
 	FILE* fp;
 	int snum,i,m=0;
 	char ch[2];
-	if(fp=fopen("data","r")==NULL)
+	if((fp=fopen("data","r"))==NULL)
 	{
-		pritntf("打开失败，请联系管理员");
-		return; 
+		printf("打开失败，请联系管理员");
+		return;
 	}
 	while(!feof(fp))
 	{
-		if(fread(&stu[m],LEN,1,fp)==1)
+		if(fread(&stu[m],LEN,1,fp))
 		{
 			m++;
 		}
 	}
 	fclose(fp);
 	if(m==0)
-	{
-		printf("无记录");
+	{   
+		printf("无记录!\n");
 		getch();
+		return; 
+	}
+	printf("请输入您要查询的学号:");
+		scanf("%d",&snum);
+		for(i=0;i<m;i++)
+			if(snum==stu[i].num)
+			{ 
+				printf("查询成功,是否显示?(y/n)");
+				scanf("%s",ch);
+				if(strcmp(ch,"Y")==0||strcmp(ch,"y")==0) 
+				{ printf("学号	姓名	C语言	高数	英语	总分\t\n");
+					printf(FORMAT,DATA);
+					getch();
+					break; 	}
+				else
+					return;
+			}   
+			if(i==m) 
+				printf("未找到您要查询的学生信息!\n");
+}
+ 
+void del()
+{
+	FILE *fp;
+	int snum,i,j,m=0;
+	char ch[2];
+	if((fp=fopen("data","a+"))==NULL)
+	{   printf("系统出错，请与开发人员联系！\n");
 		return;
 	}
-	printf("请输入你的学号");
-	scanf("%d",&snum);
-	for(i=0;i<m;i++)
-	{
-		if(snum==stu[i].num)
-		{
-			printf("查询成功，是否显示（y/n）：");
+	while(!feof(fp))  
+		if(fread(&stu[m],LEN,1,fp)==1) 
+			m++;
+		fclose(fp);
+		if(m==0) 
+		{   printf("无记录!\n");
+			return; 	}
+		printf("请输入您要删除的学号:");
+		scanf("%d",&snum);
+		for(i=0;i<m;i++)
+			if(snum==stu[i].num)
+				break;
+		 if(i==m)
+			{   printf("对不起，没有您要删除的学生信息！");
+			    return; 
+}
+			printf("确定删除?(y/n)");
 			scanf("%s",ch);
-			if(ch=="Y"||ch=="y")
+			if(strcmp(ch,"Y")==0||strcmp(ch,"y")==0)
 			{
-				printf("学号	姓名	C语言	高数	英语	总分\t\n");
-				printf(FORMAT,DATA);/*将查找出的结果按指定格式输出*/
-				getch();
-				break; 	
+				for(j=i;j<m;j++)
+					stu[j]=stu[j+1];
+					m--;
+					printf("删除成功!\n");
+					getch();
+				}
+				if((fp=fopen("data","w"))==NULL)
+				{ 
+					printf("系统出错，请与开发人员联系！\n");
+					return;
+				}
+				for(j=0;j<m;j++)
+					if(fwrite(&stu[j] ,LEN,1,fp)!=1)
+					{   printf("更新失败，请与开发人员联系!\n"); 	}
+					fclose(fp);
+}
+void change()
+{ 
+	FILE *fp;
+	int i,j,m=0,snum;
+	if((fp=fopen("data","a+"))==NULL)
+	{   printf("系统出错，请与开发人员联系！\n");
+		return;
+	}
+	while(!feof(fp))  
+		if(fread(&stu[m],LEN,1,fp)==1) 
+			m++;
+		if(m==0)
+		{   printf("无记录!\n");
+			fclose(fp);
+			return;	
+		}
+		printf("请输入您要修改的学生学号:\n");
+		scanf("%d",&snum);
+		for(i=0;i<m;i++)
+			if(snum==stu[i].num)
+				break;
+			if(i<m)
+			{	
+				printf("姓名:\n");
+				scanf("%s",stu[i].name);
+				printf("\nC语言成绩:");
+				scanf("%lf",&stu[i].c);
+				printf("\n高数:");
+				scanf("%lf",&stu[i].m);
+				printf("\n英语:");
+				scanf("%lf",&stu[i].e);
+				stu[i].sum=stu[i].c+stu[i].m+stu[i].e;
 			}
-			else
+			else 
+			{   printf("没有您要修改的学生信息!");
 				return;
 			}
-		}
-
-	}
-	if(i==m)
-		printf("未找到学生信息");
-	
+			if((fp=fopen("data","w"))==NULL)
+			{   printf("系统出错，请与开发人员联系！\n");
+				return;
+			}
+			for(j=0;j<m;j++)
+				if(fwrite(&stu[j] ,LEN,1,fp)!=1)
+				{   printf("更新失败，请与开发人员联系!");   }
+					fclose(fp);
 }
+void order()
+{ 
+	FILE *fp;
+	struct student t;
+	int i=0,j=0,m=0;
+	if((fp=fopen("data","a+"))==NULL)
+	{ 
+		printf("系统出错，请与开发人员联系！\n");
+		return;
+	}
+	while(!feof(fp)) 
+		if(fread(&stu[m] ,LEN,1,fp)==1) 
+			m++;
+		fclose(fp);
+		if(m==0) 
+		{
+			printf("无记录!\n");
+			return;
+		}
+		for(i=0;i<m-1;i++)
+			for(j=i+1;j<m;j++)
+				if(stu[i].sum<stu[j].sum)
+				{ 
+					t=stu[i];
+					stu[i]=stu[j];
+					stu[j]=t;
+				}
+				if((fp=fopen("data","w"))==NULL)
+				{ 
+					printf("系统出错，请与开发人员联系！\n");
+					getch();
+					return;
+				}
+				for(i=0;i<m;i++)
+					if(fwrite(&stu[i] ,LEN,1,fp)!=1)
+					{ 
+						printf("更新失败，请与开发人员联系！\n"); 
+					}
+					fclose(fp);
+					show();
+					getch();
+}
+void count()
+{ 
+	FILE *fp;
+	int m=0;
+	if((fp=fopen("data","r"))==NULL)
+	{         
+		printf("系统出错，请与开发人员联系！\n");
+		return;
+	}
+	while(!feof(fp))  
+		if(fread(&stu[m],LEN,1,fp)==1) 
+			m++;
+		if(m==0)
+		{
+			printf("无记录!\n");
+			fclose(fp);
+			return;
+		}
+		printf("本系统有%d条记录!\n",m);
+		getch();
+		fclose(fp);
+}
+
  
 
 
